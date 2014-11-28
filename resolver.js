e146e504080872db03eb733a7cc7658a385b6a31
Sasha Koss <koss@nocorp.me>
2014-11-28 15:40:19 +0800

    Pass extensions to resolve component function

--- resolver.js
+++ resolver.js
@@ -3,37 +3,41 @@ var path = require('path');
 // Captures component id (e.g 'feedback_form' from 'feedback/feedback_form').
 var COMPONENT_ID_PATTERN = /([^\/]+)$/;
 
-var resolveComponent = function(request, callback) {
-  var enclosingDirPath = path.join(request.path, request.request || '');
-  var captured = enclosingDirPath.match(COMPONENT_ID_PATTERN);
+var getResolveComponent = function(exts) {
+  return function(request, callback) {
+    var enclosingDirPath = path.join(request.path, request.request || '');
+    var captured = enclosingDirPath.match(COMPONENT_ID_PATTERN);
 
-  if (captured) {
-    var componentId = captured[1];
-    // TODO: Allow to use any or set of extensions
-    var componentFileName = componentId + '.jsx';
-    var componentFilePath = path.join(enclosingDirPath, componentFileName);
+    if (captured) {
+      var componentId = captured[1];
+      // TODO: Allow to use any or set of extensions
+      var componentFileName = componentId + '.jsx';
+      var componentFilePath = path.join(enclosingDirPath, componentFileName);
 
-    this.fileSystem.stat(componentFilePath, function(err, stats) {
-      if (err || !stats.isFile()) {
-        return callback();
-      }
+      this.fileSystem.stat(componentFilePath, function(err, stats) {
+        if (err || !stats.isFile()) {
+          return callback();
+        }
 
-      this.doResolve('file', {
-        path: enclosingDirPath,
-        query: request.query,
-        request: componentFileName
-      }, callback);
-    }.bind(this));
+        this.doResolve('file', {
+          path: enclosingDirPath,
+          query: request.query,
+          request: componentFileName
+        }, callback);
+      }.bind(this));
 
-  } else {
-    callback();
-  }
+    } else {
+      callback();
+    }
+  };
 };
 
-var ComponentResolverPlugin = function() {};
+var ComponentResolverPlugin = function(exts) {
+  this.exts = exts || ['jsx'];
+};
 
 ComponentResolverPlugin.prototype.apply = function(resolver) {
-  resolver.plugin('directory', resolveComponent);
+  resolver.plugin('directory', getResolveComponent(this.exts));
 };
 
 module.exports = ComponentResolverPlugin;
